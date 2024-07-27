8e6bcebce05ace8c27785ad763a18fa90328ea8f
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-07-27 14:39:28 +0530

    add type for transfer task options

--- src/types.ts
+++ src/types.ts
@@ -8,3 +8,16 @@ export interface DownloadTaskOptions {
   downloadLocation: string;
   filename: string;
 }
+
+
+export interface TransferTaskOptions {
+  sourceMongodbUri: string;
+  sourceDatabaseName: string;
+  sourceCollection: string;
+  filterQuery: object;
+  skip: number;
+  limit: number;
+  targetMongodbUri: string;
+  targetDatabaseName: string;
+  targetCollection: string;
+}
