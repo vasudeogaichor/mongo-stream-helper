4c24395a3507d9d4e1ccccb0f166b3c609e8ebe1
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-08-01 00:00:13 +0530

    move questions to separate file

---
+++ src/questions.ts
@@ -0,0 +1,103 @@
+export const transferQuestions = [
+  {
+    type: "input",
+    name: "sourceMongodbUri",
+    message: "Enter Source MongoDB URI:",
+    initial: "mongodb://localhost:27017",
+    validate: (value: string) => {
+      const uriPattern =
+        /^mongodb(?:\+srv)?:\/\/(?:[^:]+:[^@]+@)?[^:\\/]+(?:\.[^:\\/]+)*(:\d+)?(?:\/[^?]*)?(?:\?.*)?$/;
+      return uriPattern.test(value)
+        ? true
+        : "Please enter a valid MongoDB URI";
+    },
+  },
+  {
+    type: "input",
+    name: "sourceDatabaseName",
+    message: "Enter source database name:",
+    validate: (value: string) =>
+      value.trim().length > 0 || "Database name cannot be empty",
+  },
+  {
+    type: "input",
+    name: "sourceCollection",
+    message: "Enter source collection name:",
+    validate: (value: string) =>
+      value.trim().length > 0 || "Collection name cannot be empty",
+  },
+  {
+    type: "input",
+    name: "filterQuery",
+    message: "Enter filter query (JSON format):",
+    initial: "{}",
+    validate: (value: string) => {
+      try {
+        JSON.parse(value);
+        return true;
+      } catch (e) {
+        return "Please enter a valid JSON format";
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "skip",
+    message: "Enter number of documents to skip:",
+    initial: "0",
+    validate: (value: string) => {
+      const number = parseInt(value, 10);
+      return (
+        (!isNaN(number) && number >= 0) ||
+        "Please enter a valid non-negative integer"
+      );
+    },
+  },
+  {
+    type: "input",
+    name: "limit",
+    message: "Enter limit on number of documents to fetch:",
+    initial: "0", // Use 0 to fetch all documents by default
+    validate: (value: string) => {
+      const number = parseInt(value, 10);
+      return (
+        (!isNaN(number) && number >= 0) ||
+        "Please enter a valid non-negative integer"
+      );
+    },
+  },
+  {
+    type: "input",
+    name: "targetMongodbUri",
+    message: "Enter Target MongoDB URI:",
+    initial: "mongodb://localhost:27017",
+    validate: (value: string) => {
+      const uriPattern =
+        /^mongodb(?:\+srv)?:\/\/(?:[^:]+:[^@]+@)?[^:\\/]+(?:\.[^:\\/]+)*(:\d+)?(?:\/[^?]*)?(?:\?.*)?$/;
+      return uriPattern.test(value)
+        ? true
+        : "Please enter a valid MongoDB URI";
+    },
+  },
+  {
+    type: "input",
+    name: "targetDatabaseName",
+    message: "Enter target database name:",
+    validate: (value: string) =>
+      value.trim().length > 0 || "Database name cannot be empty",
+  },
+  {
+    type: "input",
+    name: "targetCollection",
+    message: "Enter target collection name:",
+    validate: (value: string) =>
+      value.trim().length > 0 || "Collection name cannot be empty",
+  },
+  {
+    type: "confirm",
+    name: "updateExisting",
+    message: "Do you want to update existing documents?:",
+    default: false,
+  },
+  // Add more questions as needed
+];
