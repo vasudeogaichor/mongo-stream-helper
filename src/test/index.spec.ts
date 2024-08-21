a3be2dccfe89022b3df3010e0f6723b6810d1515
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-08-21 23:41:49 +0530

    add test for cli interface of download task

--- src/test/index.spec.ts
+++ src/test/index.spec.ts
@@ -1,3 +1,4 @@
+import { exec } from "child_process";
 import { expect } from "chai";
 import fs from "fs";
 import path from "path";
@@ -6,7 +7,34 @@ import { transferData } from "../tasks/transfer";
 import { MongoClient } from "mongodb";
 
 describe("Mongo Stream Helper Tests", function () {
-  this.timeout(60000); // Extend timeout for longer operations
+  this.timeout(60000);
+
+  it("should download data from MongoDB using command line", function (done) {
+    this.timeout(10000); // Increase the timeout if needed
+
+    const command = `npx ts-node src/index.ts download --mongodbUri "mongodb://root:example@localhost:27017" --databaseName "testdb" --sourceCollection "testcollection1" --filterQuery "{}" --skip 0 --limit 10 --downloadLocation "/home/lord_saum/vasudeo/mongo-stream-helper/src/test" --filename "test_download.json"`;
+
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return done(error);
+      }
+
+      // console.log(`stdout: ${stdout}`);
+      // console.error(`stderr: ${stderr}`);
+
+      const filePath = path.join(
+        "/home/lord_saum/vasudeo/mongo-stream-helper/src/test",
+        "test_download.json"
+      );
+      expect(fs.existsSync(filePath)).to.be.true;
+      const fileContent = fs.readFileSync(filePath, "utf-8");
+      const downloadedDocs = JSON.parse(fileContent);
+      expect(downloadedDocs).to.have.lengthOf(10);
+
+      done();
+    });
+  });
 
   it("should download data from MongoDB to a JSON file", async function () {
     const options = {
@@ -22,10 +50,7 @@ describe("Mongo Stream Helper Tests", function () {
 
     await downloadData(options);
 
-    const filePath = path.join(
-      options.downloadLocation,
-      options.filename
-    );
+    const filePath = path.join(options.downloadLocation, options.filename);
     // console.log('filePath: ', filePath)
     expect(fs.existsSync(filePath)).to.be.true;
     const fileContent = fs.readFileSync(filePath, "utf-8");
